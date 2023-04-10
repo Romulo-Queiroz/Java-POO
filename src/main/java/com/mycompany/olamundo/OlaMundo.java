197b194592910698b8f5fa06d2c86521adf87c0a
Romulo Freitas <rfcontatosvia@gmail.com>
2023-04-10 18:41:27 -0300

    Limpando cod pra próxima aula

--- src/main/java/com/mycompany/olamundo/OlaMundo.java
+++ src/main/java/com/mycompany/olamundo/OlaMundo.java
@@ -3,9 +3,6 @@ package com.mycompany.olamundo;
 public class OlaMundo {
 
     public static void main(String[] args) {
-      ControleRemoto c = new ControleRemoto();
-      c.ligar();
-      c.abrirMenu();
-      //Aula sobre encapsulamento & polimorfismo
+      
     }
 }
