8dc6e7d4ba43a77f66c70a4eeb0adfd3893558b6
Romulo Freitas <rfcontatosvia@gmail.com>
2023-04-12 19:47:06 -0300

    Herança aula prática 02.

--- src/main/java/com/mycompany/olamundo/Main.java
+++ src/main/java/com/mycompany/olamundo/Main.java
@@ -3,13 +3,14 @@ package com.mycompany.olamundo;
 public class Main {
 
     public static void main(String[] args) {
+        // Pessoa pessoa1 = new Pessoa(); pessoa é abstrato e por isso não pode ser instanciado.
+        Visitante v1 = new Visitante("Visitante anônimo ", 0,"Indefinido");
 
-      Professor professor1 = new Professor("Gustavo", 33,"Masculino");
-      Aluno aluno1 = new Aluno("Rômulo", 25, "Masculino");
+        System.out.println("Hello"+ v1.getNome() +"você tem " + v1.getIdade() +" Anos");
 
-      professor1.setAssuntoAula("Aprendendo Herança usando JAVA.");
-      aluno1.setMatricula(3366);
+        Aluno aluno1 = new Aluno("Rômulo", 25, "Masculino");
+        aluno1.setCurso("Engenharia de Software");
+        aluno1.setMatricula(2021);
+        aluno1.fazerAniversario();
 
-      System.out.println(professor1.getAssuntoAula() + "E nosso aluno de matrícula: " + aluno1.getMatricula() + " esta acompanhando perfeitamente.");
-    }
-}
+        System.out.println("Você fez aniversário e agora tem " + aluno1.getIdade() + " Anos");
