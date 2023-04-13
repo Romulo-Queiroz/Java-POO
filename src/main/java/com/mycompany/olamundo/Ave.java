9daf49559e1ef0642262e4db9946f0ade803b5ab
Romulo Freitas <rfcontatosvia@gmail.com>
2023-04-13 20:47:53 -0300

    Polimorfismo

---
+++ src/main/java/com/mycompany/olamundo/Ave.java
@@ -0,0 +1,19 @@
+package com.mycompany.olamundo;
+
+public class Ave extends Animal{
+    private String corDaPena;
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public void emitirSom() {
+
+    }
+}
--- src/main/java/com/mycompany/olamundo/Main.java
+++ src/main/java/com/mycompany/olamundo/Main.java
@@ -3,16 +3,20 @@ package com.mycompany.olamundo;
 public class Main {
 
     public static void main(String[] args) {
-        // Pessoa pessoa1 = new Pessoa(); pessoa é abstrato e por isso não pode ser instanciado.
-        Visitante v1 = new Visitante("Visitante anônimo ", 0, "Indefinido");
 
-        System.out.println("Hello" + v1.getNome() + "você tem " + v1.getIdade() + " Anos");
+        Mamifero cachorro = new Mamifero();
+        cachorro.setIdade(3);
+        cachorro.setMembros(4);
+        cachorro.alimentar();  //polimorfismo
+        cachorro.locomover();  //polimorfismo
+        cachorro.emitirSom();  //polimorfismo
 
-        Aluno aluno1 = new Aluno("Rômulo", 25, "Masculino");
-        aluno1.setCurso("Engenharia de Software");
-        aluno1.setMatricula(2021);
-        aluno1.fazerAniversario();
 
-        System.out.println("Você fez aniversário e agora tem " + aluno1.getIdade() + " Anos");
+        Peixe peixinho = new Peixe();
+        peixinho.setIdade(4);
+        peixinho.setMembros(0);
+        peixinho.alimentar();
+        peixinho.locomover();
+        peixinho.emitirSom();
     }
 }
